25068e1d8a522d4717832475fb658089b6884c1d
John Erling Blad <jeblad@gmail.com>
2026-04-30 21:18:58 +0200

    chore: Another attempt on leverage the Lifecycle Hooks feature in commit-and-tag-version

--- include/rlog/rlog.hpp
+++ include/rlog/rlog.hpp
@@ -4,7 +4,7 @@
  */
 /**
  * RLog – A C++20 Localization and Reporting Utility
- * Version 0.0.4
+ * Version 0.0.5
  * Combined and generalized version of i18n, reporting, and logging.
  **/
 
@@ -12,8 +12,8 @@
 
 #define RLOG_VERSION_MAJOR 0
 #define RLOG_VERSION_MINOR 0
-#define RLOG_VERSION_PATCH 4
-#define RLOG_VERSION "0.0.4"
+#define RLOG_VERSION_PATCH 5
+#define RLOG_VERSION "0.0.5"
 
 #include <libintl.h>
 #include <syslog.h>
